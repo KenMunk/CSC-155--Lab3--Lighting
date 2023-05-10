5c2bd1e9c161b9f10c4353aa0f45fddad4b95de6
Kenneth Munk <da.munkk@gmail.com>
2023-05-09 22:21:13 -0700

    Loading references

---
+++ Prog14_6_wood/code/Code.java
@@ -0,0 +1,309 @@
+package code;
+
+import java.io.*;
+import java.lang.Math;
+import java.nio.*;
+import java.util.*;
+import java.awt.Color;
+import javax.swing.*;
+import static com.jogamp.opengl.GL4.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.texture.*;
+import com.jogamp.common.nio.Buffers;
+import org.joml.*;
+
+public class Code extends JFrame implements GLEventListener
+{
+	private GLCanvas myCanvas;
+
+	private int renderingProgram;
+	private int[] vao = new int[1];
+	private int[] vbo = new int[3];
+
+	private ImportedModel dolphinObj;
+	private int numDolphinVertices;
+	
+	private float cameraX, cameraY, cameraZ;
+	private float objLocX, objLocY, objLocZ;
+	private Vector3f lightLoc = new Vector3f(-2.0f, 5.0f, 1.0f);
+	
+	// allocate variables for display() function
+	private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);
+	private Matrix4f pMat = new Matrix4f();  // perspective matrix
+	private Matrix4f vMat = new Matrix4f();  // view matrix
+	private Matrix4f mMat = new Matrix4f();  // model matrix
+	private Matrix4f mvMat = new Matrix4f(); // model-view matrix
+	private Matrix4f invTrMat = new Matrix4f(); // inverse-transpose
+	private Matrix4f texRotMat = new Matrix4f(); // rotation for wood texture
+	private int mLoc, vLoc, pLoc, nLoc, texRotLoc;
+	private int globalAmbLoc, ambLoc, diffLoc, specLoc, posLoc, mambLoc, mdiffLoc, mspecLoc, mshiLoc;
+	private float aspect;
+	private Vector3f currentLightPos = new Vector3f();
+	private float[] lightPos = new float[3];
+
+	private int noiseTexture;
+	private int noiseWidth = 256;
+	private int noiseHeight= 256;
+	private int noiseDepth = 256;
+	private double[][][] noise = new double[noiseWidth][noiseHeight][noiseDepth];
+	private java.util.Random random = new java.util.Random();
+	
+	// white light
+	float[] globalAmbient = new float[] { 0.5f, 0.5f, 0.5f, 1.0f };
+	float[] lightAmbient = new float[] { 0.0f, 0.0f, 0.0f, 1.0f };
+	float[] lightDiffuse = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+	float[] lightSpecular = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+		
+	// wood material
+	float[] matAmb = new float[] {0.5f, 0.3f, 0.15f, 1.0f};
+	float[] matDif = new float[] {0.5f, 0.3f, 0.15f, 1.0f};
+	float[] matSpe = new float[] {0.5f, 0.3f, 0.15f, 1.0f};
+	float matShi = 15.0f;
+
+	public Code()
+	{	setTitle("Chapter 14 - program6");
+		setSize(1200, 800);
+		myCanvas = new GLCanvas();
+		myCanvas.addGLEventListener(this);
+		this.add(myCanvas);
+		this.setVisible(true);
+	}
+
+	public void display(GLAutoDrawable drawable)
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		gl.glClear(GL_COLOR_BUFFER_BIT);
+		gl.glClear(GL_DEPTH_BUFFER_BIT);
+		
+		gl.glUseProgram(renderingProgram);
+		
+		mLoc = gl.glGetUniformLocation(renderingProgram, "m_matrix");
+		vLoc = gl.glGetUniformLocation(renderingProgram, "v_matrix");
+		pLoc = gl.glGetUniformLocation(renderingProgram, "p_matrix");
+		nLoc = gl.glGetUniformLocation(renderingProgram, "norm_matrix");
+		texRotLoc = gl.glGetUniformLocation(renderingProgram, "texRot_matrix");
+		
+		vMat.identity().setTranslation(-cameraX,-cameraY,-cameraZ);
+		
+		mMat.identity();
+		mMat.translate(objLocX, objLocY, objLocZ);
+		mMat.rotateY((float)Math.toRadians(50.0f));
+		
+		currentLightPos.set(lightLoc);
+		installLights();
+
+		mMat.invert(invTrMat);
+		invTrMat.transpose(invTrMat);
+
+		gl.glUniformMatrix4fv(mLoc, 1, false, mMat.get(vals));
+		gl.glUniformMatrix4fv(vLoc, 1, false, vMat.get(vals));
+		gl.glUniformMatrix4fv(pLoc, 1, false, pMat.get(vals));
+		gl.glUniformMatrix4fv(nLoc, 1, false, invTrMat.get(vals));
+		gl.glUniformMatrix4fv(texRotLoc, 1, false, texRotMat.get(vals));
+		
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+		gl.glEnableVertexAttribArray(0);
+		
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+		gl.glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+		gl.glEnableVertexAttribArray(1);
+		
+		gl.glActiveTexture(GL_TEXTURE0);
+		gl.glBindTexture(GL_TEXTURE_3D, noiseTexture);
+		
+		gl.glEnable(GL_CULL_FACE);
+		gl.glFrontFace(GL_CCW);
+		gl.glEnable(GL_DEPTH_TEST);
+		gl.glDepthFunc(GL_LEQUAL);
+
+		gl.glDrawArrays(GL_TRIANGLES, 0, numDolphinVertices);
+	}
+
+	public void init(GLAutoDrawable drawable)
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		dolphinObj = new ImportedModel("dolphinLowPoly.obj");
+		renderingProgram = Utils.createShaderProgram("code/vertShader.glsl", "code/fragShader.glsl");
+
+		float aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
+		pMat.identity().setPerspective((float) Math.toRadians(50.0f), aspect, 0.1f, 1000.0f);
+
+		setupVertices();
+		cameraX = 0.0f; cameraY = 0.0f; cameraZ = 1.3f;
+		objLocX = -0.15f; objLocY = 0.0f; objLocZ = 0.0f;
+		
+		generateNoise();	
+		noiseTexture = buildNoiseTexture();
+	
+		texRotMat.rotateY((float)Math.toRadians(10.0f));	
+		texRotMat.rotateX((float)Math.toRadians(10.0f));
+		texRotMat.rotateZ((float)Math.toRadians(10.0f));
+	}
+	
+	private void setupVertices()
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+	
+		numDolphinVertices = dolphinObj.getNumVertices();
+		Vector3f[] vertices = dolphinObj.getVertices();
+		Vector2f[] texCoords = dolphinObj.getTexCoords();
+		Vector3f[] normals = dolphinObj.getNormals();
+		
+		float[] pvalues = new float[numDolphinVertices*3];
+		float[] tvalues = new float[numDolphinVertices*2];
+		float[] nvalues = new float[numDolphinVertices*3];
+		
+		for (int i=0; i<numDolphinVertices; i++)
+		{	pvalues[i*3]   = (float) (vertices[i]).x();
+			pvalues[i*3+1] = (float) (vertices[i]).y();
+			pvalues[i*3+2] = (float) (vertices[i]).z();
+			tvalues[i*2]   = (float) (texCoords[i]).x();
+			tvalues[i*2+1] = (float) (texCoords[i]).y();
+			nvalues[i*3]   = (float) (normals[i]).x();
+			nvalues[i*3+1] = (float) (normals[i]).y();
+			nvalues[i*3+2] = (float) (normals[i]).z();
+		}
+		
+		gl.glGenVertexArrays(vao.length, vao, 0);
+		gl.glBindVertexArray(vao[0]);
+		gl.glGenBuffers(vbo.length, vbo, 0);
+		
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(pvalues);
+		gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf, GL_STATIC_DRAW);
+
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		FloatBuffer texBuf = Buffers.newDirectFloatBuffer(tvalues);
+		gl.glBufferData(GL_ARRAY_BUFFER, texBuf.limit()*4, texBuf, GL_STATIC_DRAW);
+
+		gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+		FloatBuffer norBuf = Buffers.newDirectFloatBuffer(nvalues);
+		gl.glBufferData(GL_ARRAY_BUFFER, norBuf.limit()*4,norBuf, GL_STATIC_DRAW);
+	}
+	
+	private void installLights()
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		
+		lightPos[0]=currentLightPos.x(); lightPos[1]=currentLightPos.y(); lightPos[2]=currentLightPos.z();
+		
+		// get the locations of the light and material fields in the shader
+		globalAmbLoc = gl.glGetUniformLocation(renderingProgram, "globalAmbient");
+		ambLoc = gl.glGetUniformLocation(renderingProgram, "light.ambient");
+		diffLoc = gl.glGetUniformLocation(renderingProgram, "light.diffuse");
+		specLoc = gl.glGetUniformLocation(renderingProgram, "light.specular");
+		posLoc = gl.glGetUniformLocation(renderingProgram, "light.position");
+		mambLoc = gl.glGetUniformLocation(renderingProgram, "material.ambient");
+		mdiffLoc = gl.glGetUniformLocation(renderingProgram, "material.diffuse");
+		mspecLoc = gl.glGetUniformLocation(renderingProgram, "material.specular");
+		mshiLoc = gl.glGetUniformLocation(renderingProgram, "material.shininess");
+	
+		//  set the uniform light and material values in the shader
+		gl.glProgramUniform4fv(renderingProgram, globalAmbLoc, 1, globalAmbient, 0);
+		gl.glProgramUniform4fv(renderingProgram, ambLoc, 1, lightAmbient, 0);
+		gl.glProgramUniform4fv(renderingProgram, diffLoc, 1, lightDiffuse, 0);
+		gl.glProgramUniform4fv(renderingProgram, specLoc, 1, lightSpecular, 0);
+		gl.glProgramUniform3fv(renderingProgram, posLoc, 1, lightPos, 0);
+		gl.glProgramUniform4fv(renderingProgram, mambLoc, 1, matAmb, 0);
+		gl.glProgramUniform4fv(renderingProgram, mdiffLoc, 1, matDif, 0);
+		gl.glProgramUniform4fv(renderingProgram, mspecLoc, 1, matSpe, 0);
+		gl.glProgramUniform1f(renderingProgram, mshiLoc, matShi);
+	}
+
+	// 3D Texture section
+	
+	private void fillDataArray(byte data[])
+	{ double xyPeriod = 30.0;
+	  double turbPower = 0.15;
+	  double maxZoom =  40.0;
+	  
+	  for (int i=0; i<noiseWidth; i++)
+	  { for (int j=0; j<noiseHeight; j++)
+	    { for (int k=0; k<noiseDepth; k++)
+	      {	double xValue = (i - (double)noiseWidth/2.0) / (double)noiseWidth;
+		double yValue = (j - (double)noiseHeight/2.0) / (double)noiseHeight;
+		double distanceFromZ = Math.sqrt(xValue * xValue + yValue * yValue)
+						+ turbPower * turbulence(i, j, k, maxZoom) / 256.0;
+		double sineValue = 128.0 * Math.abs(Math.sin(2.0 * xyPeriod * distanceFromZ * Math.PI));
+
+		Color c = new Color((int)(60+(int)sineValue), (int)(10+(int)sineValue), 0);
+
+	        data[i*(noiseWidth*noiseHeight*4)+j*(noiseHeight*4)+k*4+0] = (byte) c.getRed();
+	        data[i*(noiseWidth*noiseHeight*4)+j*(noiseHeight*4)+k*4+1] = (byte) c.getGreen();
+	        data[i*(noiseWidth*noiseHeight*4)+j*(noiseHeight*4)+k*4+2] = (byte) c.getBlue();
+	        data[i*(noiseWidth*noiseHeight*4)+j*(noiseHeight*4)+k*4+3] = (byte) 255;
+	} } } }
+
+	private int buildNoiseTexture()
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+
+		byte[] data = new byte[noiseWidth*noiseHeight*noiseDepth*4];
+		
+		fillDataArray(data);
+
+		ByteBuffer bb = Buffers.newDirectByteBuffer(data);
+
+		int[] textureIDs = new int[1];
+		gl.glGenTextures(1, textureIDs, 0);
+		int textureID = textureIDs[0];
+
+		gl.glBindTexture(GL_TEXTURE_3D, textureID);
+
+		gl.glTexStorage3D(GL_TEXTURE_3D, 1, GL_RGBA8, noiseWidth, noiseHeight, noiseDepth);
+		gl.glTexSubImage3D(GL_TEXTURE_3D, 0, 0, 0, 0,
+				noiseWidth, noiseHeight, noiseDepth, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, bb);
+		
+		gl.glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		return textureID;
+	}
+
+	void generateNoise()
+	{	for (int x=0; x<noiseWidth; x++)
+		{	for (int y=0; y<noiseHeight; y++)
+			{	for (int z=0; z<noiseDepth; z++)
+				{	noise[x][y][z] = random.nextDouble();
+	}	}	}	}
+	
+	double smoothNoise(double zoom, double x1, double y1, double z1)
+	{	//get fractional part of x, y, and z
+		double fractX = x1 - (int) x1;
+		double fractY = y1 - (int) y1;
+		double fractZ = z1 - (int) z1;
+
+		//neighbor values that wrap
+		double x2 = x1 - 1; if (x2<0) x2 = (Math.round(noiseWidth / zoom)) - 1;
+		double y2 = y1 - 1; if (y2<0) y2 = (Math.round(noiseHeight / zoom)) - 1;
+		double z2 = z1 - 1; if (z2<0) z2 = (Math.round(noiseDepth / zoom)) - 1;
+
+		//smooth the noise by interpolating
+		double value = 0.0;
+		value += fractX       * fractY       * fractZ       * noise[(int)x1][(int)y1][(int)z1];
+		value += (1.0-fractX) * fractY       * fractZ       * noise[(int)x2][(int)y1][(int)z1];
+		value += fractX       * (1.0-fractY) * fractZ       * noise[(int)x1][(int)y2][(int)z1];	
+		value += (1.0-fractX) * (1.0-fractY) * fractZ       * noise[(int)x2][(int)y2][(int)z1];
+				
+		value += fractX       * fractY       * (1.0-fractZ) * noise[(int)x1][(int)y1][(int)z2];
+		value += (1.0-fractX) * fractY       * (1.0-fractZ) * noise[(int)x2][(int)y1][(int)z2];
+		value += fractX       * (1.0-fractY) * (1.0-fractZ) * noise[(int)x1][(int)y2][(int)z2];
+		value += (1.0-fractX) * (1.0-fractY) * (1.0-fractZ) * noise[(int)x2][(int)y2][(int)z2];
+		
+		return value;
+	}
+
+	private double turbulence(double x, double y, double z, double maxZoom)
+	{	double sum = 0.0, zoom = maxZoom;
+		while(zoom >= 0.9)
+		{	sum = sum + smoothNoise(zoom, x/zoom, y/zoom, z/zoom) * zoom;
+			zoom = zoom / 2.0;
+		}
+		sum = 128.0 * sum / maxZoom;
+		return sum;
+	}
+
+	public static void main(String[] args) { new Code(); }
+	public void dispose(GLAutoDrawable drawable) {}
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height)
+	{	float aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
+		pMat.identity().setPerspective((float) Math.toRadians(50.0f), aspect, 0.1f, 1000.0f);
+	}
+}
